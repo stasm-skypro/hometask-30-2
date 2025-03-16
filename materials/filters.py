0f5935f4fc21ffa372d43070f348dc4891ba85ce
Станислав Маяцкий <stasm226@gmail.com>
2025-03-16 14:01:57 +0500

    Add filter for API Endpoint Paiment model

---
+++ materials/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+from users.models import Payment
+
+class PaymentFilter(django_filters.FilterSet):
+    """Фильтр для модели Payment."""
+
+    course = django_filters.NumberFilter(field_name="course__id", lookup_expr="exact")
+    lesson = django_filters.NumberFilter(field_name="lesson__id", lookup_expr="exact")
+    payment_method = django_filters.ChoiceFilter(choices=Payment.PAYMENT_METHODS)
+    ordering = django_filters.OrderingFilter(fields=["date"])
+
+    class Meta:
+        model = Payment
+        fields = ["course", "lesson", "payment_method"]
